3e903cb9a6b4d76c78332b28eedfd48dca36b8e2
Joshua Okhiria <65799346+josh869@users.noreply.github.com>
2024-06-09 18:31:30 +0100

    Update and rename DISTINCT key world - remove duplicates SQL to DISTINCY key world - remove duplicates.sql

--- DISTINCY key world - remove duplicates.sql
+++ DISTINCY key world - remove duplicates.sql
@@ -3,7 +3,6 @@ USE AdventureWorks2012;
 --selecting dataset to work with.
 SELECT * FROM DupTestTable
 
-
 -- This data has no duplicates, total number of rows and total number of distinct rows are the same
 SELECT 
 	COUNT(*)  AS TotalNumberRow
@@ -13,8 +12,7 @@ SELECT COUNT(*) AS TotalDistinctRow
 FROM 
 (SELECT DISTINCT firstname, lastname, emailaddress FROM DupTestTable) as A
 
-
--- mauallhy inserting duplicate data into the dataset
+-- manually inserting duplicate data into the dataset
 INSERT INTO DupTestTable VALUES ('Syed', 'Abbas', 'syed0@adventure-works.com'),
 ('Syed', 'Abbas', 'syed0@adventure-works.com'),
 ('Syed', 'Abbas', 'syed0@adventure-works.com'),
@@ -26,7 +24,6 @@ INSERT INTO DupTestTable VALUES ('Syed', 'Abbas', 'syed0@adventure-works.com'),
 ('Syed', 'Abbas', 'syed0@adventure-works.com'),
 ('Syed', 'Abbas', 'syed0@adventure-works.com')
 
-
 -- This data now has duplicates, total number of rows is now higher compared to the total number os distinct rows.
 SELECT 
 	COUNT(*)  AS TotalNumberRow
@@ -36,10 +33,8 @@ SELECT COUNT(*) AS TotalDistinctRow
 FROM 
 (SELECT DISTINCT firstname, lastname, emailaddress FROM DupTestTable) as A
 
-
 -- Removing duplicates by using the distinct keyword and copying the output into a new dataset
 SELECT * INTO NO_DupTestTable FROM (SELECT DISTINCT firstname, lastname, emailaddress FROM DupTestTable) AS A
-
 
 -- dataset has no duplicates, all duplicates has been filtered out
 SELECT * FROM NO_DupTestTable
